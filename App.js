ec5166d54856521150049fcd3332f6ace7f6570f
Stephen Karpeles <stephen@stephenkarpeles.com>
2021-07-02 13:42:41 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Test from './components/Test'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +33,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <div>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', { name: 'Jane' })
+        }
+      />
+      <Button
+        title="Go to Jeff's profile"
+        onPress={() =>
+          navigation.navigate('Profile', { name: 'Jeff' })
+        }
+      />
+    </div>
+  );
+};
+
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
